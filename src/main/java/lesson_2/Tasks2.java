e07b66f4635f7e07633c6549700ba351a7742c60
sanjar <sanjar171766@gmail.com>
2025-09-25 20:04:31 +0500

    some tasks

--- src/main/java/lesson_2/Tasks2.java
+++ src/main/java/lesson_2/Tasks2.java
@@ -1,5 +1,7 @@
 package lesson_2;
 
+import java.math.BigInteger;
+
 public class Tasks2 {
     public static void print100(){
         for (int i = 1; i <= 100; i++) {
@@ -71,15 +73,67 @@ public class Tasks2 {
             numbersArray[i] = Character.getNumericValue(tmp.charAt(i));
         }
 
+        int left = 0;
+        int right = numbersArray.length - 1;
 
+        while (left < right) {
+            int temp = numbersArray[left];
+            numbersArray[left] = numbersArray[right];
+            numbersArray[right] = temp;
+            left++;
+            right--;
+        }
+
+        for (int nmb : numbersArray) {
+            System.out.println(nmb);
+        }
     }
 
+    // In this method I was informed, that I should use BigInt with working very big numbers
+    public static void findFactorial(int n) {
+        if (n < 0) {
+            System.out.println("Invalid data!");
+        } else {
+            BigInteger result = BigInteger.ONE;
+            for (int i = 1; i <= n; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            }
+            System.out.println(result);
+        }
+    }
+
+    public static void findFirstNumberWith7Division(int[] numbers) {
+        int result = 0;
+        for (int tmp : numbers) {
+            if (tmp % 7 == 0 && tmp > 1000) {
+                result = tmp;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+//    public static void printPrimeNumbers(int[] numbers){
+//        for (int tmp : numbers) {
+//            for (int i = 3; i <= Math.sqrt(tmp); i += 2) {
+//                if (tmp % i == 0) {
+//                    System.out.println(tmp);
+//                }
+//            }
+//        }
+//    }
+
     public static void main(String[] args) {
+        int[] numbers = {7, 1000, 1001, 1010, 1008};
+
 //        Tasks2.print100();
 //        Tasks2.sumN(10);
-//        Tasks2.multiplicationN(3);
+//        Tasks2.multiplicationN(20);
 //        Tasks2.sumEvenN(4);
 //        Tasks2.sumNumbers(1234);
-
+//        Tasks2.reversalNumbers(1234);
+//        Tasks2.findFactorial(20);
+//        Tasks2.findFirstNumberWith7Division(numbers);
+//        Tasks2.printPrimeNumbers(numbers);
     }
 }
