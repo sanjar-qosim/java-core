936acbf6e9d34740b70442840b37487f3b2c61f5
Sanjar <sanjar171766@gmail.com>
2025-10-01 02:16:52 +0500

    Complete the Fighter task

--- src/main/java/lesson_3/StreetFighter/Fighter.java
+++ src/main/java/lesson_3/StreetFighter/Fighter.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @Getter
 public class Fighter {
 
-    private ArrayList<Fighter> fighters = new ArrayList<>();
+    private static ArrayList<Fighter> fighters = new ArrayList<>();
 
     private static int codeCounter;
     private int code;
@@ -27,7 +28,8 @@ public class Fighter {
         fighters.add(this);
     }
 
-    public void getFighters() {
+    public static void getFighters() {
+        System.out.println("All fighters: ");
         for (Fighter tmp : fighters) {
             System.out.println(tmp);
         }
@@ -41,15 +43,49 @@ public class Fighter {
         }
     }
 
-    public void fight() {
+    public static void fight(Fighter fighter1, Fighter fighter2) {
+        Random random = new Random();
+        int round = 0;
 
+        Fighter firstAttacker = null;
+        Fighter secondAttacker = null;
+
+        if (random.nextBoolean()) {
+            firstAttacker = fighter1;
+            secondAttacker = fighter2;
+        } else {
+            firstAttacker = fighter2;
+            secondAttacker = fighter1;
+        }
+
+        System.out.println("The fight is beginning...");
+        System.out.println(firstAttacker.name + " attacks first.\n");
+
+        while (fighter1.health > 0 && fighter2.health > 0) {
+            System.out.println("Round: " + ++round);
+
+            secondAttacker.health -= firstAttacker.attack;
+            System.out.println("The " + firstAttacker.name + " is attacking for " + firstAttacker.attack + " damage.\nThe " + secondAttacker.name + " health is " + secondAttacker.health);
+            if (secondAttacker.health <= 0) {
+                System.out.println("The " + firstAttacker.name + " is win!");
+                break;
+            }
+
+            firstAttacker.health -= secondAttacker.attack;
+            System.out.println("The " + secondAttacker.name + " is attacking for " + secondAttacker.attack + " damage.\nThe " + firstAttacker.name + " health is " + firstAttacker.health);
+            if (firstAttacker.health <= 0) {
+                System.out.println("The " + secondAttacker.name + " is win!");
+                break;
+            }
+            System.out.println();
+        }
     }
 
     @Override
     public String toString() {
         return "Fighter{" +
-                "fighters=" + fighters +
-                ", code=" + code +
+                "fighters=" +
+                "code=" + code +
                 ", name='" + name + '\'' +
                 ", health=" + health +
                 ", attack=" + attack +
